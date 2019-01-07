dc6738afedf15fbd1d1757e07b1b0e94d4676952
wingter <wentai_wu@126.com>
2019-01-07 18:41:12 +0000

    1. Minor modification on Global parameters annotation format;

--- detect_algos/detect_EDK-means.py
+++ detect_algos/detect_EDK-means.py
@@ -10,14 +10,17 @@ from sklearn.cluster import KMeans
 import matplotlib.pyplot as plot
 
 ''' global params '''
-# the range of data to learn
 data_file = "data_std.txt"
 data_form = 'std'
+# define data structure for Jiuzhou Log data (normalized/standardized)
+# i.e., timestamp(str), CROND(float), RSYSLOGD(float), SESSION(float), SSHD(float), SU(float)
 JZLogFrame_type = np.dtype([('timestamp', 'S13'), ('CROND', 'f8'), ('RSYSLOGD', 'f8'),
                             ('SESSION', 'f8'), ('SSHD', 'f8'), ('SU', 'f8')])
+# the range of data to learn
 start_date = "2018-06-29-00"
 start_h = 0
 end_h = 3457
+# slotting
 slot_size = 3
 num_models = 24/slot_size if 24%slot_size == 0 else 24/slot_size + 1
 
